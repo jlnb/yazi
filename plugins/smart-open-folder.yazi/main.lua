155dfa216509c9f630c7bef99a8f1a3e828406e0
jlnb <github@julianbelz.com>
2026-01-05 10:10:50 +0100

    Try to fix smart-open-folder plugin - so far without success

--- plugins/smart-open-folder.yazi/main.lua
+++ plugins/smart-open-folder.yazi/main.lua
@@ -3,11 +3,35 @@ return {
 	entry = function()
 		local h = cx.active.current.hovered
 		if h and h.cha.is_dir then
-			ya.mgr_emit("open", {})
+			ya.emit("explorer", {})
 		else
-			ya.mgr_emit("leave", {})
-			ya.mgr_emit("open", {})
-			ya.mgr_emit("enter", {})
+			ya.emit("leave", {})
+			ya.emit("explorer", {})
+			ya.emit("enter", {})
 		end
 	end,
 }
+
+--return {
+--    entry = function()
+--        local h = cx.active.current.hovered
+--        if not h then return end
+--
+--        -- Zielpfad ermitteln
+--        local target = tostring(h.url)
+--        if not h.cha.is_dir then
+--            target = tostring(cx.active.current.cwd)
+--        end
+--
+--        -- OS-Check über globale Konstanten (robuster in 2026)
+--        if ya.target_family() == "windows" then
+--            ya.sh(string.format('explorer "%s"', target))
+--        elseif ya.target_family() == "macos" then
+--            ya.sh(string.format('open "%s"', target))
+--        else
+--            -- Für Linux: Probiere xdg-open
+--            ya.sh(string.format('xdg-open "%s"', target))
+--        end
+--    end,
+--}
+
