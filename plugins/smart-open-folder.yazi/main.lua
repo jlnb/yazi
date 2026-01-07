3e91d3056c88b3c3c9881b300084dfbe1c1fcb47
jlnb <github@julianbelz.com>
2026-01-07 07:56:04 +0100

    Fix yazi.toml after version upgrade

--- plugins/smart-open-folder.yazi/main.lua
+++ plugins/smart-open-folder.yazi/main.lua
@@ -3,35 +3,11 @@ return {
 	entry = function()
 		local h = cx.active.current.hovered
 		if h and h.cha.is_dir then
-			ya.emit("explorer", {})
+			ya.emit("open", { hovered = true, use = "openfolder" })
 		else
 			ya.emit("leave", {})
-			ya.emit("explorer", {})
+			ya.emit("open", { hovered = true, use = "openfolder" })
 			ya.emit("enter", {})
 		end
 	end,
 }
-
---return {
---    entry = function()
---        local h = cx.active.current.hovered
---        if not h then return end
---
---        -- Zielpfad ermitteln
---        local target = tostring(h.url)
---        if not h.cha.is_dir then
---            target = tostring(cx.active.current.cwd)
---        end
---
---        -- OS-Check über globale Konstanten (robuster in 2026)
---        if ya.target_family() == "windows" then
---            ya.sh(string.format('explorer "%s"', target))
---        elseif ya.target_family() == "macos" then
---            ya.sh(string.format('open "%s"', target))
---        else
---            -- Für Linux: Probiere xdg-open
---            ya.sh(string.format('xdg-open "%s"', target))
---        end
---    end,
---}
-
